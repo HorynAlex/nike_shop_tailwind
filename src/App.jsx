fa731f7b3cda2439cf9c48229135d89a07eb2834
HorynAlex <horyn.oleksandr@gmail.com>
2025-10-04 18:46:23 +0300

    deploying project nike shop tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <main className="relative">
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">SpecialOffers</section>
+    </main>
+  );
+}
+
+export default App;

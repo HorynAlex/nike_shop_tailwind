1717f11a0e693640057a1b2b63eeed9d4d9599ce
HorynAlex <horyn.oleksandr@gmail.com>
2025-10-05 15:04:00 +0300

    almost finished project

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -16,7 +16,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   return (
     <div
       className={`border-2 rounded-xl ${
-        bigShoeImg === imgURL ? "border-coral-red" : "border-transperent"
+        bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"
       } cursor-pointer max-sm:flex-1`}
       onClick={handleCLick}
     >
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { footerLogo } from "../assets/images";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="" width={150} height={46} />
+          </a>
+          <p>
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
